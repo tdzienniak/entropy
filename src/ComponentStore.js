177a24a4fe4ff13842ac1cdbe6618ecf9ce30a3b
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2016-11-27 12:37:03 +0100

    Add ComponentStore docs

--- src/ComponentStore.js
+++ src/ComponentStore.js
@@ -13,38 +13,75 @@ import Component from './Component';
 const ComponentStore = compose({
   init() {
     /**
-    * [_greatestComponentId description]
-    * @type {Number}
-    */
+     * Stores the gratest component's ID so far. Every component type when registered gets it's own unique ID.
+     *
+     * @private
+     * @memberof Component#
+     * @name _greatestComponentId
+     * @type {Number}
+     */
     this._greatestComponentId = 0;
 
     /**
-    * [_componentsIdsMap description]
-    * @type {Object}
-    */
+     * Stores map of registered types to IDs.
+     *
+     * @example
+     * {
+     *   Position: 0,
+     *   Velocity: 1,
+     *   ...
+     * }
+     *
+     * @private
+     * @memberof Component#
+     * @name _componentsIdsMap
+     * @type {Object}
+     */
     this._componentsIdsMap = {};
 
     /**
-    * [_pools description]
-    * @type {Object}
-    */
+     * Object with instances of Pool, one for each component type. Type is a key.
+     *
+     * @private
+     * @memberof Component#
+     * @name _pools
+     * @type {Object}
+     */
     this._pools = {};
 
     /**
-    * [_initializers description]
-    * @type {Object}
-    */
+     * Object with components factories. Factories are created when component is registerd. Type is a key.
+     *
+     * @private
+     * @memberof Component#
+     * @name _factories
+     * @type {Object}
+     */
     this._factories = {};
   },
   methods: {
     /**
      * Registers component.
      *
+     * @example
+     * game.component.register({
+     *   type: 'Position',
+     *   onCreate(x, y) {
+     *     this.x = x;
+     *     this.y = y;
+     *   },
+     * });
+     *
+     * const c = game.component.create('Position', 1, 2);
+     * c.x; // 1
+     * c.y; // 2
+     *
      * @public
      * @memberof ComponentStore#
      * @method register
-     * @param  {String} type   [description]
-     * @param  {Function} initFn [description]
+     * @param {Object}    descriptor            object describing component
+     * @param {String}    descriptor.type       type of components
+     * @param {Function}  descriptor.onCreate   method called when component is created
      */
     register(descriptor) {
       this._factories[descriptor.type] = compose(Component, {
@@ -74,26 +111,37 @@ const ComponentStore = compose({
       });
     },
     /**
-     * [registerComponents description]
-     * @param  {[type]} obj [description]
-     * @return {[type]}     [description]
+     * Registeres many components.
+     *
+     * @public
+     * @memberof ComponentStore#
+     * @method registerMany
+     * @param  {Array} descriptiors array of component's descriptors (see {@link ComponentStore#register})
      */
     registerMany(descriptors) {
       descriptors.forEach(descriptor => this.register(descriptor));
     },
     /**
-     * [createComponent description]
-     * @param  {[type]} name    [description]
-     * @param  {[type]} ...args [description]
-     * @return {[type]}         [description]
+     * Creates new component instance or aquires one from pool.
+     *
+     * @public
+     * @memberof ComponentStore#
+     * @method create
+     * @param  {String} type    type of component to create
+     * @param  {...Any} ...args arguments passed to `onCreate` method
+     * @return {Component}      new (or reused) component ready to add to entity
      */
     create(type, ...args) {
       return this._pools[type].allocate(...args);
     },
     /**
-     * [freeComponent description]
-     * @param  {[type]} component [description]
-     * @return {[type]}           [description]
+     * Frees component. Component is added to the pool.
+     * This method is called internally by the engine, user should not call it.
+     *
+     * @public
+     * @memberof ComponentStore#
+     * @method free
+     * @param  {Component} component component
      */
     free(component) {
       this._pools[component.getType()].free(component);
