701b07748ed38f242f63d88ade24fbbaf6c5c6fc
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2016-12-23 14:52:05 +0100

    Add SystemStore docs

--- packages/entropy/src/SystemStore.js
+++ packages/entropy/src/SystemStore.js
@@ -3,21 +3,49 @@ import pickBy from 'lodash/pickBy';
 import { isFunction } from './helpers';
 import System from './System';
 
+/**
+ * Centralized place for registering and creating systems.
+ *
+ * __For internal use only!__
+ *
+ * @class SystemStore
+ * @param {Object}          opts
+ * @param {Entropy}         opts.game game instance
+ */
 const SystemStore = compose({
   init(opts) {
-    this.game = opts.game;
     /**
-    * [_initializers description]
-    * @type {Object}
-    */
+     * Game instance injected to constructor.
+     *
+     * @public
+     * @memberOf SystemStore#
+     * @name game
+     * @type Entropy
+     */
+    this.game = opts.game;
+
+    /**
+     * Object with factories for system types.
+     *
+     * @private
+     * @memberOf SystemStore#
+     * @name _factories
+     * @type Object
+     */
     this._factories = {};
   },
   methods: {
     /**
-     * [registerSystem description]
-     * @param  {[type]} name   [description]
-     * @param  {[type]} initFn [description]
-     * @return {[type]}        [description]
+     * Registers system.
+     *
+     * @public
+     * @memberof SystemStore#
+     * @method register
+     * @param {Object}    descriptor            object describing system
+     * @param {String}    descriptor.type       type of system
+     * @param {Function}  descriptor.onCreate   method called when system is created
+     * @param {Function}  descriptor.onRemove   method called when system is removed
+     * @param {Function}  descriptor.onUpdate   method called when system is updated
      */
     register(descriptor) {
       this._factories[descriptor.type] = compose(System, {
@@ -28,18 +56,25 @@ const SystemStore = compose({
       });
     },
     /**
-     * [registerSystems description]
-     * @param  {[type]} obj [description]
-     * @return {[type]}     [description]
+     * Registeres many systems.
+     *
+     * @public
+     * @memberof SystemStore#
+     * @method registerMany
+     * @param  {Array} descriptiors array of system's descriptors (see {@link SystemStore#register})
      */
     registerMany(descriptors) {
       descriptors.forEach(descriptor => this.register(descriptor));
     },
     /**
-     * [createSystem description]
-     * @param  {[type]} name    [description]
-     * @param  {[type]} ...args [description]
-     * @return {[type]}         [description]
+     * Creates new entity instance or acquires one from pool.
+     *
+     * @public
+     * @memberof SystemStore#
+     * @method create
+     * @param  {String} type    type of system to create
+     * @param  {...Any} args arguments passed to `onCreate` method
+     * @return {System}         new system ready to be added to engine
      */
     create(type, ...args) {
       const system = this._factories[type]({
